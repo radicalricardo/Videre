2addf62831305280f160a98ba0d9e812cdc4885e
DiogoFreeDom <diogomsmendes@hotmail.com>
2021-05-12 23:12:07 +0100

    interação com a bd expandida, guarda frames a função de guardar frames recebe parâmetros:  uma frame (provavelmente uma blob de opencv, não sei se funciona como está);  id do utilizador (que estou a pensar guardar algures na sessão, sendo que se busca no login, para poupar interações com a bd);  timestamp (int que depois é convertido para os formatos corretos).

--- videredb.py
+++ videredb.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine, text
 import flask
 import bcrypt
+import time
 
 engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/Videre', echo=False, future=True)
 
+
+# UTILIZADOR
 
 def inserirUtilizador(username, password):
     hashedPW = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
@@ -23,5 +26,24 @@ def verificaUtilizador(username, password):
             return False
 
 
-#inserirUtilizador("DM","1234")
-print(verificaUtilizador("DMSM", "1234"))
+# FRAMES
+
+
+def guardaFrame(frame, userid, timestamp):
+    frameName = f"{userid}-{time.strftime('%Y%m%d_%H%M%S', time.gmtime(timestamp))}"
+    framePath = f"frames/{frameName}"
+    newFrame = open(framePath + ".jpg", "x")
+    newFrame.write(frame)
+    with engine.connect() as con:
+        con.execute(text(f"INSERT INTO frames (timestamp, user_id, frame_path) "
+                         f"VALUES ('{time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(timestamp))}', {userid}, '{frameName}')"))
+        con.commit()
+
+
+# OBJECT_FOUND
+
+
+def guardaObjectos(frameid, objectid, caixa: list, confianca):      # tenho de pensar se fica incluido no #frames ou não
+    pass
+
+
