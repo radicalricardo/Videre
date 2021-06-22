bd0f3f294c271ecd267d99ea3f3a3101960c482e
brunu97 <brunuo@live.com.pt>
2021-06-22 02:02:50 +0100

    Sistema de carregar ficheiros
    pagina e sistema de carregar imagens e videos

--- VidereApp/carregarFicheiros.py
+++ VidereApp/carregarFicheiros.py
@@ -1,3 +1,7 @@
+import os
+
+import cv2
+import numpy
 from flask import Blueprint, request, render_template, url_for, flash
 from werkzeug.utils import redirect
 
@@ -6,4 +10,27 @@ carregarFicheiros_pagina = Blueprint('carregaFicheiro', __name__, template_folde
 
 @carregarFicheiros_pagina.route('/carregar', methods=["POST", "GET"])
 def carregaFicheiros():
-    return render_template("carregarFicheiro.html")
+
+    if request.method == "POST":
+        if 'file' not in request.files:
+            flash('Pedido feito não possui ficheiro.')
+            return redirect(url_for("inicio"))
+
+        file = request.files['file']
+        if file.filename == '':
+            flash('Nenhum ficheiro selecionado')
+            return redirect(url_for("inicio"))
+
+        if file and not file.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.mp4')):
+            flash('Formato do ficheiro não é suportado. Use apenas PNG, JPG, JPEG ou MP4')
+            return redirect(url_for("inicio"))
+
+        if not file.filename.lower().endswith('mp4'): # É uma imagem
+            frame = cv2.imdecode(numpy.frombuffer(request.files['file'].read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
+            cv2.imwrite('Imagem.jpg', frame)
+        else: # É um video
+            file.save(os.path.join("videoTeste.mp4"))
+        return render_template("carregarFicheiro.html")
+
+    else:
+        return render_template("carregarFicheiro.html")
