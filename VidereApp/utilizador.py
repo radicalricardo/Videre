4ee9f7ee06fed72e637f8d0031170fc838a86c75
brunu97 <brunuo@live.com.pt>
2021-06-25 01:55:50 +0100

    Processamento de Fotos

--- VidereApp/utilizador.py
+++ VidereApp/utilizador.py
@@ -4,6 +4,8 @@ import time
 import uuid
 import cv2
 import numpy as np
+from flask import send_from_directory
+
 import dataset
 import config
 import videredb
@@ -96,7 +98,6 @@ class Camara:
 
     def processa(self):
         while True:
-            print("Proc")
             ativo, frame = self.imagem.read()
             if not ativo: break
 
@@ -195,3 +196,101 @@ class Camara:
                 self.tempoPassado = 0
             else:
                 self.tempoPassado = time.time() - self.tempoInicial
+
+
+class Imagem:
+    def __init__(self, id_user, filtros):
+        self.filtros = filtros
+        self.id_user = id_user
+        self.imagem = cv2.VideoCapture(0, 0)
+        self.net = cv2.dnn.readNet(config.yoloPath, config.yoloPathWeights)
+
+        # Inicia CUDA, se utilizador não suportar, estas linhas são ignoradas
+        if cv2.cuda.getCudaEnabledDeviceCount() > 0:
+            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+            self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+        else:
+            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
+            self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+    def processar(self):
+        ativo, frame = self.imagem.read()
+        tamanho = frame.shape
+        layer_names = self.net.getLayerNames()
+        outputlayers = [layer_names[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+
+        altura = frame.shape[0]
+        comprimento = frame.shape[1]
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255, (512, 512), swapRB=True, crop=False)
+        self.net.setInput(blob)
+        processados = self.net.forward(outputlayers)
+
+        class_ids = []
+        confidences = []  # Grau de confiança sobre a imagem
+        caixas = []
+        for p in processados:
+            for ObjetoApanhado in p:
+                pontuacoes = ObjetoApanhado[5:]
+                class_id = np.argmax(pontuacoes)
+
+                if class_id not in self.filtros:  # Este objeto detetado não está presente nos filtros para detetar
+                    continue
+
+                certeza = pontuacoes[class_id]
+
+                if certeza > 0.5:
+                    # Obtem posição (eu não percebo a magia negra que o net.forward faz, mas as posições das coisas estão ai)
+                    centroX = int(ObjetoApanhado[0] * comprimento)
+                    centroY = int(ObjetoApanhado[1] * altura)
+                    c = int(ObjetoApanhado[2] * comprimento)
+                    a = int(ObjetoApanhado[3] * altura)
+                    x = int(centroX - c / 2)
+                    y = int(centroY - a / 2)
+
+                    caixas.append([x, y, c, a])
+                    confidences.append(float(certeza))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(caixas, confidences, 0.5, 0.4)
+
+        objetos_captuados_frame = []
+        for i in indexes:
+            objeto_no_frame = {}
+            i = i[0]
+            caixa = caixas[i]
+            x = caixa[0]
+            y = caixa[1]
+            w = caixa[2]
+            h = caixa[3]
+            objeto_no_frame["object_id"] = class_ids[i]
+            objeto_no_frame["confianca"] = confidences[i]
+            objeto_no_frame["topLeft"] = [x, y]
+            objeto_no_frame["bottomRight"] = [w, h]
+            objetos_captuados_frame.append(objeto_no_frame)
+            label = str(dataset.classes[class_ids[i]])
+            cor = dataset.classes_cores[class_ids[i]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), cor, 2)
+
+            # Impede que texto fique fora do ecrã
+            if y < 40:  # se o X estiver 40 pixeis perto do topo da imagem
+                y += 45  # Baixa 45 pixeis
+            if x < 5: x += 5
+
+            if x + (len(label) + 5) * 15 > tamanho[1]:  # Impede que o texto saia para o lado da imagem
+                x -= (len(label) + 5) * 15
+
+            # Faz um texto em baixo que serve como contorno
+            cv2.putText(frame, label + " " + str(round(confidences[i], 2)), (x - 1, y - 10),
+                        cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 2, lineType=cv2.LINE_AA)
+
+            # Nome do objeto, texto de cima
+            cv2.putText(frame, label + " " + str(round(confidences[i], 2)), (x, y - 10), cv2.FONT_HERSHEY_DUPLEX, 1,
+                        cor, 1, lineType=cv2.LINE_AA)
+
+        # Converte para jpg
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        tempo = time.time()
+        nomeFrame = f"{self.id_user}-{time.strftime('%Y%m%d_%H%M%S', time.gmtime(tempo))}"
+        videredb.guardaFrame(frame, self.id_user, tempo, objetos_captuados_frame)
+        return send_from_directory("Frames", nomeFrame + ".png")
